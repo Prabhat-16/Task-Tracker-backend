07941f18c5bb6885d6bd7073da541832cfdaaa26
Prabhat Mishra <mprabhat1607@gmail.com>
2025-04-16 18:04:04 +0530

    added simple route

--- server.js
+++ server.js
@@ -8,6 +8,12 @@ const PORT = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(bodyParser.json());
+
+// Sample route
+app.get('/api', (req, res) => {
+  res.json({ message: 'Welcome to the Task Tracker API!' });
+});
+
 app.use('/api/tasks', taskRoutes);
 
 app.listen(PORT, () => {
